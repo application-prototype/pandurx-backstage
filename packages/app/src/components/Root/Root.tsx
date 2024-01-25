1db8fa9315309d28f592f1404bf05c120f0fbe3c
Christine <internalfire5@live.com>
2024-01-24 20:42:09 -0500

    Update Root.tsx

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -7,6 +7,7 @@ import LibraryBooks from '@material-ui/icons/LibraryBooks';
 import CreateComponentIcon from '@material-ui/icons/AddCircleOutline';
 import LogoFull from './LogoFull';
 import LogoIcon from './LogoIcon';
+import BuildIcon from '@material-ui/icons/Build';
 import {
   Settings as SidebarSettings,
   UserSettingsSignInAvatar,
@@ -69,6 +70,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
         <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
+        <SidebarItem icon={BuildIcon} to="entity-validation" text="Validator" />
         {/* End global nav */}
         <SidebarDivider />
         <SidebarScrollWrapper>
